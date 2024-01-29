7102425e953a507d257f148cbec483a4849a5750
Chantal Pic <chantal.pic@ec.gc.ca>
2024-01-29 17:25:22 -0500

    Use new location of SPS database

--- scripts/link-dbase.sh
+++ scripts/link-dbase.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# link to database at CMC
+# link to SPS database at CMC
 
-DOMAIN=`hostname -d`
 
-if [[ "${DOMAIN}"  = cmc.ec.gc.ca ]]; then
-    SPS_DBASE=/users/dor/armn/sps/SPS_MODEL_DFILES
-    [ -e ${SPS_DBASE} ] && [ ! -e sps_dbase ] && ln -sf ${SPS_DBASE} sps_dbase
-elif [[ ${DOMAIN} = "collab.science.gc.ca" ]]; then
-    SPS_DBASE=/space/hall0/work/eccc/mrd/rpnenv/smsh001
-    [ -e ${SPS_DBASE} ] && [ ! -e sps_dbase ] && ln -sf ${SPS_DBASE} sps_dbase
-elif [[ -z "${DOMAIN}" || ${DOMAIN} = "science.gc.ca" ]]; then
-    SPS_DBASE=/home/ssps121/SPS_MODEL_DFILES
-    [ -e ${SPS_DBASE} ] && [ ! -e sps_dbase ] && ln -sf ${SPS_DBASE} sps_dbase
+SPS_DBASE=/fs/ssm/eccc/mrd/rpn/models/sps/sps-data/dfiles
+
+if [ -e ${SPS_DBASE} ] ; then
+    if  [ ! -e sps_dbase ] ; then
+        ln -sf ${SPS_DBASE} sps_dbase
+    else
+        echo "There is already a link to a database: please make sure it's the correct link:"
+        ls -al sps_dbase
+    fi
 else
-    echo "hostname not found: don't know where database is."
+    echo "Database not found: don't know where database is."
 fi
