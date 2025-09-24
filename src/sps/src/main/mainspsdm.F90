632331b9ae62ccf541b2b73468e587e951f5c652
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@ec.gc.ca>
2025-09-24 16:48:51 +0000

    Added new F90 file replacing the .inc

---
+++ src/sps/src/main/mainspsdm.F90
@@ -0,0 +1,16 @@
+program sps
+   use app
+   use iso_fortran_env
+   implicit none
+
+#include <sps_build_info.h>
+
+   app_ptr=app_init(0,PROJECT_NAME_STRING,VERSION,PROJECT_DESCRIPTION_STRING,BUILD_TIMESTAMP)
+   call app_libregister(APP_LIBVGRID,HAVE_VGRID//c_null_char)
+   call app_libregister(APP_LIBTDPACK,HAVE_TDPACK//c_null_char)
+   call app_libregister(APP_LIBPHY,phy_VERSION//c_null_char)
+   call app_libregister(APP_LIBMDLUTIL,modelutils_VERSION//c_null_char)
+
+   call spsdm
+end program sps
+
